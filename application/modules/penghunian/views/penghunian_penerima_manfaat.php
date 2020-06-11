855fa1613d3668c8fdaf642bcfecebbe55cbb517
ahmadisme <ahmadneverlast@gmail.com>
2020-06-11 11:08:39 +0700

    update

--- application/modules/penghunian/views/penghunian_penerima_manfaat.php
+++ application/modules/penghunian/views/penghunian_penerima_manfaat.php
@@ -47,13 +47,25 @@
                     <div class="nav-wrapper">
                         <ul class="nav nav-pills nav-fill flex-column flex-md-row " id="tabs-icons-text" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link mb-sm-3 mb-md-0 lebar100 active" id="tabs-icons-text-1-tab" data-toggle="tab" href="#tabs-icons-text-1" role="tab" aria-controls="tabs-icons-text-1" aria-selected="true"><i class="ni ni-cloud-upload-96 mr-2"></i>Huni</a>
+                                <a class="nav-link mb-sm-3 mb-md-0 bg-gradient-info lebar100 active" id="tabs-icons-text-1-tab" data-toggle="tab" href="#tabs-icons-text-1" role="tab" aria-controls="tabs-icons-text-1" aria-selected="true"><img src="<?= base_url('assets') ?>/img/flat.png" width="42" height="50">
+                                    <h3>
+                                        <font color="white"> Huni</font>
+                                    </h3>
+                                </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link mb-sm-3 mb-md-0 lebar100" id="tabs-icons-text-2-tab" data-toggle="tab" href="#tabs-icons-text-2" role="tab" aria-controls="tabs-icons-text-2" aria-selected="false"><i class="ni ni-bell-55 mr-2"></i>Belum huni</a>
+                                <a class="nav-link mb-sm-3 mb-md-0 bg-gradient-info lebar100" id="tabs-icons-text-2-tab" data-toggle="tab" href="#tabs-icons-text-2" role="tab" aria-controls="tabs-icons-text-2" aria-selected="false"><img src="<?= base_url('assets') ?>/img/flat.png" width="42" height="50">
+                                    <h3>
+                                        <font color="white"> Belum Huni</font>
+                                    </h3>
+                                </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link mb-sm-3 mb-md-0 lebar100" id="tabs-icons-text-3-tab" data-toggle="tab" href="#tabs-icons-text-3" role="tab" aria-controls="tabs-icons-text-3" aria-selected="false"><i class="ni ni-calendar-grid-58 mr-2"></i>Tahun Anggaran</a>
+                                <a class="nav-link mb-sm-3 mb-md-0 bg-gradient-info lebar100" id="tabs-icons-text-3-tab" data-toggle="tab" href="#tabs-icons-text-3" role="tab" aria-controls="tabs-icons-text-3" aria-selected="false"><img src="<?= base_url('assets') ?>/img/flat.png" width="42" height="50">
+                                    <h3>
+                                        <font color="white"> Tahun Anggaran</font>
+                                    </h3>
+                                </a>
                             </li>
 
                         </ul>
